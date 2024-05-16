1438b01ef45c7c4aeee557d26e33b7ad5513e996
vtrsantos <victor.rsantos@sptech.school>
2024-05-15 22:20:54 -0300

    commit do teste

---
+++ src/test/java/sptech/school/exemploactions/service/CalculadoraServiceTest.java
@@ -0,0 +1,30 @@
+package sptech.school.exemploactions.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class CalculadoraServiceTest {
+
+    @Test
+    @DisplayName("Quando acionado com 10 e 2, então deve retornar 5")
+    public void testDividir(){
+
+        // Arrange
+        CalculadoraService calculadoraService = new CalculadoraService();
+        double a = 10;
+        double b = 2;
+        double esperado = 5;
+
+        // Act
+        double resultado = calculadoraService.dividir(a,b);
+
+        // Assert
+        assertEquals(esperado, resultado);
+
+    }
+
+
+}
