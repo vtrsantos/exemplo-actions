3e876f34c9a51a0e8231d6b65967a7f3eb65b947
vtrsantos <victor.rsantos@sptech.school>
2024-05-16 11:45:46 -0300

    Should theow exception

--- src/test/java/sptech/school/exemploactions/service/CalculadoraServiceTest.java
+++ src/test/java/sptech/school/exemploactions/service/CalculadoraServiceTest.java
@@ -2,26 +2,46 @@ package sptech.school.exemploactions.service;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.web.server.ResponseStatusException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
-class CalculadoraServiceTest {
+    class CalculadoraServiceTest {
 
-    @Test
-    @DisplayName("Quando acionado com 10 e 2, então deve retornar 5")
-    public void testDividir(){
+        @Test
+        @DisplayName("Quando acionado com 10 e 2, então deve retornar 5")
+        public void testDividir(){
 
-        // Arrange
-        CalculadoraService calculadoraService = new CalculadoraService();
-        double a = 10;
-        double b = 2;
-        double esperado = 5;
+            // Arrange
+            CalculadoraService calculadoraService = new CalculadoraService();
+            double a = 10;
+            double b = 2;
+            double esperado = 5;
 
-        // Act
-        double resultado = calculadoraService.dividir(a,b);
+            // Act
+            double resultado = calculadoraService.dividir(a,b);
 
-        // Assert
-        assertEquals(esperado, resultado);
+            // Assert
+            assertEquals(esperado, resultado);
+        }
+
+
+        @Test
+        @DisplayName("Quando acionado com 10 e 0, então deve lançar uma exceção")
+        public void testDividirPorZero(){
+
+            // Arrange
+            CalculadoraService calculadoraService = new CalculadoraService();
+            double a = 10;
+            double b = 0;
+            var expectedMessage = "400 BAD_REQUEST \"Divisão por zero não permitida\"";
+
+            // Assert
+            ResponseStatusException exception = assertThrows(ResponseStatusException.class, () -> {calculadoraService.dividir(a,b);});
+
+            // Assert
+            assertEquals(expectedMessage, exception.getMessage());
+
+        } 
     }
-}
